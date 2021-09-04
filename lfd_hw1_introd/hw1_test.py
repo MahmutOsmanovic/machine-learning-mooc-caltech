c5d105c7b0e6ee7a4520699cd6c8772ce35840e4
MahmutOsmanovic <47375043+MahmutOsmanovic@users.noreply.github.com>
2021-09-04 03:31:10 +0200

    hw1

---
+++ lfd_hw1_introd/hw1_test.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb  2 12:21:09 2021
+
+@author: Mahmu
+"""
+
+import random
+import pylab
+import numpy as np
+
+x1 = random.uniform(-1, 1)
+y1 = random.uniform(-1, 1)
+print(str(x1) + "\n" + str(y1))
